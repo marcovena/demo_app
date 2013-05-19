1f91d54cb8964c0279ab177fffbc00e5c44d5a3f
marcovena <marcovena@libero.it>
2013-05-19 16:28:20 +0200

    adde cooment

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,5 +2,5 @@ class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
   validates :content, :length => { :maximum => 140 }
 
-  belongs_to :user
+  belongs_to :user #comment
 end

34eb0b306634d15391d098dc37075de768db51a5
marcovena <marcovena@libero.it>
2013-05-23 16:59:41 +0200

    Finita configurazione statica

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 def full_title(page_title)
 base="Ruby on RAILS TUTORIAL"
-full_title = page_title.empty? ? base : "#{base} | #{full_title}"
+full_title = page_title.empty? ? base : "#{base} | #{page_title}"
 end
 end

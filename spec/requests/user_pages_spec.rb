cfcd26daab6d3b2aceb9ee85a91a0f994e1f7749
marcovena <marcovena@libero.it>
2013-05-28 19:32:31 +0200

    User sign up with validation

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,7 +4,7 @@ describe "User pages" do
   subject {page}
 
 
- describe "Profile page" do 
+  describe "Profile page" do 
   	let(:user) {FactoryGirl.create(:user)}
   	before {visit user_path(user)}
 
@@ -14,8 +14,25 @@ describe "User pages" do
 
   describe "signup page" do 
   	before {visit signup_path}
-  it {should have_content('Sign up')}
-  it {should have_title(full_title('Sign up'))}
-  end
 
-end
+    let(:submit) { "Create my account"}
+
+    describe "with invalid information" do 
+      it "should not create a user" do 
+        expect {click_button "Create my account"}.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",     with:"MyExampleUser"
+        fill_in "email",      with:"MyExampleUser@domain.com"
+        fill_in "Password",     with:"MyExampleUser"
+        fill_in "Confirmation",     with:"MyExampleUser"
+      end
+      it "should create a user" do
+        expect {click_button "Create my account"}.to change(User, :count).by(1)
+      end
+    end
+  end
+end 

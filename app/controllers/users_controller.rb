d2db6e4c2175c83bad61c290af27c97674da31c6
marcovena <marcovena@libero.it>
2013-06-09 13:04:41 +0200

    progress on editing profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+before_filter :correct_user,   only: [:edit, :update]
+
   def new
   	@user = User.new 
   end
@@ -18,9 +21,38 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+
+  end
+
+def update
+  @user = User.find(params[:id])
+  if @user.update_attributes(params[:user])
+    #successful update
+    flash[:success] = "Profile Updated"
+    sign_in(@user)
+    redirect_to @user
+  else
+    render 'edit'
+  end
+
+end
 
   private
   	def user_params
   		params.require(user).permit(:name, :email, :password, :password_confirmation)
   	end
+
+    def signed_in_user
+      if !signed_in?
+      redirect_to signin_url, notice: "Please sign in"
+      end
+    end
+
+    def correct_user(user)
+      @user = User.find(params[:id])
+      redirect_to root_home unless user == @user
+    end
+
 end

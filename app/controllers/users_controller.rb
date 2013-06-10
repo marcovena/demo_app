b25bce1822aaaa6c56528b9bb5a8d5845af8d0b9
marcovena <marcovena@libero.it>
2013-06-10 16:59:55 +0200

    added pagination and deletion of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,34 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index,:edit, :update]
+  before_filter :signed_in_user, only: [:index,:edit, :update, :destroy]
   before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
 
 #display all users
 def index
-  @users = User.paginate(page: params[:page])
+  @users = User.paginate(page:params[:page])
 end
 
-  def new
-  	@user = User.new 
-  end
+def new
+ @user = User.new 
+end
 
-  def create
-  	@user = User.new(params[:user])
-  	if @user.save
-      sign_in @user
-      flash[:success] = "Welcome #{@user.name.upcase}"
-  		redirect_to @user
-  	else
-  		render 'new'
-  	end
-  end
+def create
+ @user = User.new(params[:user])
+ if @user.save
+  sign_in @user
+  flash[:success] = "Welcome #{@user.name.upcase}"
+  redirect_to @user
+else
+  render 'new'
+end
+end
 
-  def show
-  	@user = User.find(params[:id])
-  end
+def show
+ @user = User.find(params[:id])
+end
 
-  def edit
-  end
+def edit
+end
 
 def update
   @user = User.find(params[:id])
@@ -42,21 +43,32 @@ def update
 
 end
 
-  private
-  	def user_params
-  		params.require(user).permit(:name, :email, :password, :password_confirmation)
-  	end
+def destroy
+  del_user = User.find(params[:id])
+  del_user.destroy
+  flash[:success] = "User removed"
+  redirect_to users_url
+end
 
-    def signed_in_user
-      if !signed_in?
-        store_location
-      redirect_to signin_url, notice: "Please sign in"
-      end
-    end
+private
+def user_params
+  params.require(user).permit(:name, :email, :password, :password_confirmation)
+end
 
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to root_path unless current_user?(@user)
-    end
+def signed_in_user
+  if !signed_in?
+    store_location
+    redirect_to signin_url, notice: "Please sign in"
+  end
+end
+
+def correct_user
+  @user = User.find(params[:id])
+  redirect_to root_path unless current_user?(@user)
+end
+
+def admin_user
+  redirect_to root_path unless current_user.admin?
+end
 
 end

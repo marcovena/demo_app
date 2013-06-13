c22f1997c72a351388d766eb0c8bc41e73b753f8
marcovena <marcovena@libero.it>
2013-06-13 12:37:22 +0200

    Finished integration of microposts.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,101 +1,35 @@
 class MicropostsController < ApplicationController
   # GET /microposts
   # GET /microposts.json
-  before_filter :signed_in_user
+  before_filter :signed_in_user, only: [:create, :destroy]
+  before_filter :correct_user, only: :destroy
 
 
-  def index
-    @microposts = Micropost.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @microposts }
-    end
-  end
-
-  # GET /microposts/1
-  # GET /microposts/1.json
-  def show
-    @micropost = Micropost.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @micropost }
-    end
-  end
-
-  # GET /microposts/new
-  # GET /microposts/new.json
-  def new
-    @micropost = Micropost.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @micropost }
-    end
-  end
-
-  # GET /microposts/1/edit
-  def edit
-    @micropost = Micropost.find(params[:id])
-  end
-
-  # POST /microposts
-  # POST /microposts.json
-=begin
-def create
-    @micropost = Micropost.new(params[:micropost])
-
-    respond_to do |format|
-      if @micropost.save
-        format.html { redirect_to @micropost, notice: 'Micropost was successfully created.' }
-        format.json { render json: @micropost, status: :created, location: @micropost }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @micropost.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-=end
-
-def create
-  @micropost = current_user.microposts.build(params[:micropost])
-  if @micropost.save
-    flash[:success] = "Micropost successfully created"
-    redirect_to root_url
-  else
-    flash[:error] = "Error creating micropost: #{@micropost.errors.full_messages}"
+  def create
+    @micropost = current_user.microposts.build(params[:micropost])
+    if @micropost.save
+      flash[:success] = "Micropost successfully created"
+      redirect_to root_url
+    else
+      flash[:error] = "Error creating micropost: #{@micropost.errors.full_messages}"
     #render 'static_pages/home'
     redirect_to root_url
   end
-  
 end
-
-  # PUT /microposts/1
-  # PUT /microposts/1.json
-  def update
-    @micropost = Micropost.find(params[:id])
-
-    respond_to do |format|
-      if @micropost.update_attributes(params[:micropost])
-        format.html { redirect_to @micropost, notice: 'Micropost was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @micropost.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # DELETE /microposts/1
   # DELETE /microposts/1.json
   def destroy
-    @micropost = Micropost.find(params[:id])
     @micropost.destroy
+    redirect_to root_url
 
-    respond_to do |format|
-      format.html { redirect_to microposts_url }
-      format.json { head :no_content }
-    end
   end
+
+  private
+  def correct_user
+    @micropost = current_user.microposts.find_by_id(params[:id])
+    redirect_to root_url if @micropost.nil?
+
+  end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
 
 		if user && user.authenticate(params[:session][:password])
 			sign_in user
-			redirect_back_or(user)
+			redirect_back_or(root)
 		else 
 			#erroreù
 			flash.now[:error] = "Invalid credentials"

63a6995535d77f04c086408bebabf228913a0843
marcovena <marcovena@libero.it>
2013-06-14 18:26:45 +0200

    Created followers models

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,10 +16,21 @@ class User < ActiveRecord::Base
 	attr_accessible :name, :email, :password, :password_confirmation
 	has_secure_password
 
+
+#dependencies
 	has_many :microposts, dependent: :destroy
+	
+	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+	has_many :reverse_relationships, foreign_key: "followed_id" , class_name:  "Relationship",
+	dependent:   :destroy
+
+	has_many :followed_users, through: :relationships, source: :followed
+	has_many :followers, through: :reverse_relationships, source: :follower
+
+
+# VALIDATION
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-#can't be null fields  
 	validates :name, presence: true , length: { maximum: 50 }
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
 	uniqueness: { case_sensitive: false }
@@ -32,6 +43,18 @@ class User < ActiveRecord::Base
 
 def feed
 	Micropost.where("user_id = ?", id)
+end
+
+def following?(other_user)
+	relationships.find_by_followed_id(other_user.id)
+end
+
+def follow!(other_user)
+	relationships.create!(followed_id: other_user.id)
+end
+
+def unfollow!(other_user)
+	relationships.find_by_followed_id(other_user.id).destroy
 end
 
 private
